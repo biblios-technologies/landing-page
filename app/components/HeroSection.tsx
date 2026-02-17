49c15da8f4add9a6666a55339d4c4aa194e38455
biblios-technologies <hello@bibliosbible.com>
2026-02-16 23:28:56 -0500

    fix logo and sizing

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,11 +1,6 @@
 import { Button } from "./ui/button";
 import { ArrowRight } from "lucide-react";
-import b1 from '../images/final_B1.png';
-import b2 from '../images/final_B2.png';
-import i from '../images/final_i.png';
-import l from '../images/final_L.png';
-import o from '../images/final_O.png';
-import s from '../images/final_S.png';
+import BibliosLogo from '../images/Biblios Official Logo.png';
 
 
 
@@ -33,52 +28,32 @@ const HeroSection = ({ onScrollToContact }: HeroSectionProps) => {
           </div>
 
           {/* Headline */}
-          <div className="flex flex-col gap-2 justify-center items-center mt-12">
+          <div className="flex flex-col gap-2 justify-center items-center mt-8 md:mt-12">
             <h1 className={`ont-bold welcome-size flex justify-center gap-1`}>
               <span><img
-                  src={b1}
+                  src={BibliosLogo}
                   alt="Biblios Logo"
-                  width={105}
-                />
-              </span>
-              <span><img
-                  src={i}
-                  alt="Biblios Logo"
-                  width={46}
-                />
-              </span>
-              <span><img
-                  src={b2}
-                  alt="Biblios Logo"
-                  width={105}
-                />
-              </span>
-              <span><img
-                  src={l}
-                  alt="Biblios Logo"
-                  width={86}
-                />
-              </span>
-              <span><img
-                  src={i}
-                  alt="Biblios Logo"
-                  width={46}
-                />
-              </span>
-              <span><img
-                  src={o}
-                  alt="Biblios Logo"
-                  width={108}
-                />
-              </span>
-              <span><img
-                  src={s}
-                  alt="Biblios Logo"
-                  width={106}
+                  className="
+                    w-56
+                    sm:w-72
+                    md:w-[420px]
+                    lg:w-[600px]
+                    h-auto
+                  "
                 />
               </span>
             </h1>
-            <p className="text-3xl mt-2 font-normal text-[#fdc61d]" style={{ letterSpacing: "0.40em" }}>TECHNOLOGIES</p>
+            <p className="
+              mt-2
+              font-normal
+              text-[#fdc61d]
+              tracking-[0.25em]
+              text-lg
+              sm:text-xl
+              md:text-2xl
+              lg:text-3xl
+            "
+            style={{ letterSpacing: "0.40em" }}>TECHNOLOGIES</p>
           </div>
 
           {/* Subheadline */}
@@ -108,7 +83,7 @@ const HeroSection = ({ onScrollToContact }: HeroSectionProps) => {
       </div>
 
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-6 inset-x-0 flex justify-center animate-bounce">
         <div className="w-6 h-10 rounded-full border-2 border-black/20 flex items-start justify-center p-2">
           <div className="w-1 h-2 bg-[#30b7f9]/75 rounded-full" />
         </div>
